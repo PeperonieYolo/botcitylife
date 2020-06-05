c0bd0475f4610e9538c11a1221a4e8492466a7ab
PeperonieYolo <66439798+PeperonieYolo@users.noreply.github.com>
2020-06-05 12:34:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+const tokan = require("./tokan.json");
+const colours = require("./colours.json");
+const superagent = require("superagent");
+const dateFormat = require('dateformat');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+bot.on("ready", () => {
+    // This event will run if the bot starts, and logs in, successfully.
+    console.log(`Bot allumé`); 
+    // Example of changing the bot's playing game to something useful. `client.user` is what the
+    // docs refer to as the "ClientUser".
+    bot.user.setActivity(`*help`);
+  });
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = tokan.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
+
+bot.login(tokan.token);
